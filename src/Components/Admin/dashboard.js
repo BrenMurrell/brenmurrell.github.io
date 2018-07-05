ededebbc24afc05c59de8d188630eebc3be63701
BrenMurrell <bren.murrell@nvinteractive.com>
2018-07-06 09:10:42 +1200

    scaffolding user dashboard

---
+++ src/Components/Admin/dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+
+    render() {
+        return(
+            <h1>Admin Dashboard</h1>
+        )
+    }
+}
+
+export default Dashboard;
