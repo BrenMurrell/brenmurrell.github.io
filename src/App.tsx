5e47e32057b79837f0d5d2f59cc97328ca4d4026
Bren Murrell <bren@vil.nz>
2025-03-24 12:35:07 +1300

    Add Storypark

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Outlet } from "react-router-dom"
 import Navigation from "./components/Navigation/Navigation"
-
+import Footer from './Footer'
 
 function App() {
   return (
     <>
         <Navigation />
         <Outlet />
+        <Footer />
     </>
   )
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+              <span>&copy; 2025</span> | 
+              <a href="mailto:brenmurrell@gmail.com?subject=Contact from brenmurell.github.io">Email me</a> | 
+              <a href="tel:+64-27-467-0548">Call me</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
